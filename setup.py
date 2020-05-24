778ef45e3d3dfaed77a3cbd5caf8642d97c02cde
MichailZh <37882050+MichailZh@users.noreply.github.com>
2020-05-24 23:36:35 +0200

    V1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     author='MichailZh',
     author_email='michail.zhukov@gmail.com',
-    url='git@github.com:MichailZh/Iconator.git',
+    url='https://github.com/MichailZh/Iconator.git',
     packages=['iconator'],
     zip_safe=False
 )
