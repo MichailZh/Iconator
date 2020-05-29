b57506b809265a85defee9d0605d6ab6f29b9dad
MichailZh <37882050+MichailZh@users.noreply.github.com>
2020-05-29 16:48:03 +0200

    Pack.data2.9.13

--- Iconator/templatetags/iconator.py
+++ Iconator/templatetags/iconator.py
@@ -1,10 +1,14 @@
 from django import template
+import os.path
 
 register = template.Library()
 
 
 @register.simple_tag()
 def iconator(name):
-    with open("Iconator/" + "icons" + "/" + name + ".svg", 'r') as file:
+    rel_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(rel_path, "Iconator/icons"+ name + ".svg")
+    with open(path, 'r') as file:
         data = file.read()
+        print(path)
         return data
