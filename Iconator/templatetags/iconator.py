3b69f2f4aae4b1c4794ef6507116117a338d9b57
MichailZh <37882050+MichailZh@users.noreply.github.com>
2020-05-31 16:51:31 +0200

    Icons 0.1.1

--- Iconator/templatetags/iconator.py
+++ Iconator/templatetags/iconator.py
@@ -7,7 +7,7 @@ register = template.Library()
 @register.simple_tag()
 def iconator(name):
     rel_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(rel_path, "icons"+ "\\" + name + ".svg")
+    path = os.path.join(rel_path, "icons"+ "/" + name + ".svg")
     with open(path, 'r') as file:
         data = file.read()
         return data
