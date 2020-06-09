54194a60bf9ff849f784e65634d9db0dcaa64973
MichailZh <37882050+MichailZh@users.noreply.github.com>
2020-06-09 16:52:46 +0200

    Iconatron 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iconatron',
-    version='0.0.1',
+    version='0.1.3',
     description='icons package for django',
     license='MIT',
     author='MichailZh',
